97fc0054429592255d2d950bea833b5fe06bb807
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2023-12-07 15:59:52 +0600

    added new api

--- Assets/Packages/com.studio23.ss2.achievementsystem/Runtime/Data/IDTableMapper.cs
+++ Assets/Packages/com.studio23.ss2.achievementsystem/Runtime/Data/IDTableMapper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Studio23.SS2.AchievementSystem.Data
@@ -7,6 +8,10 @@ namespace Studio23.SS2.AchievementSystem.Data
     public class IDTableMapper : ScriptableObject
     {
         public List<IDMap> IDMaps;
+        public string GetMappedID(string id)
+        {
+           return  IDMaps.First(r => r.Key == id).Value;
+        }
     }
 
 
