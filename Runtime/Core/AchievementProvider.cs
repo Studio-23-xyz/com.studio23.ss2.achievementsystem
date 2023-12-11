e744100b545c7f26054fc681953ad2abd73d66ae
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2023-12-11 12:25:19 +0600

    ## [v1.1.4] - 2023-12-11
    ### Added
    - Get Achievement Method
    
    ### Fixed
    - An editor script bug that didn't show stats features

---
+++ Runtime/Core/AchievementProvider.cs
@@ -0,0 +1,17 @@
+using Studio23.SS2.AchievementSystem.Data;
+using System;
+using UnityEngine;
+
+namespace Studio23.SS2.AchievementSystem.Providers
+{
+	public  abstract class AchievementProvider : MonoBehaviour
+	{
+		[SerializeField] protected IDTableMapper _achievementMapper;
+        [SerializeField] protected IDTableMapper _statsMapper;
+        public abstract void Initialize();
+		public abstract void UnlockAchievement(string achievementIdentifier);
+        public abstract float GetStat(string statName);
+        public abstract void SetStat(string statName, float value);
+        public abstract bool IsAchievementUnlocked(string achievementName);
+    }
+}
---
+++ Runtime/Core/AchievementSystem.cs
@@ -0,0 +1,67 @@
+using Studio23.SS2.AchievementSystem.Providers;
+using UnityEngine;
+
+namespace Studio23.SS2.AchievementSystem.Core
+{
+    public class AchievementSystem : MonoBehaviour
+    {
+        public static AchievementSystem Instance;
+
+        public AchievementProvider _achievementProvider;
+
+        private void Awake()
+        {
+            Instance = this;
+            Initialize();
+        }
+
+
+        /// <summary>
+        /// Initialize Provider
+        /// </summary>
+        public void Initialize()
+        {
+            _achievementProvider = GetComponent<AchievementProvider>();
+            _achievementProvider?.Initialize();
+        }
+
+
+        public bool IsAchievementUnlocked(string achievementName)
+        {
+            return _achievementProvider.IsAchievementUnlocked(achievementName);
+        }
+
+
+        /// <summary>
+        /// Unlocks The achievement
+        /// </summary>
+        /// <param name="achievementName"></param>
+        public  void UnlockAchievement(string achievementName)
+        {
+           _achievementProvider.UnlockAchievement(achievementName);
+        }
+
+        /// <summary>
+        /// Sets the value of the stat
+        /// </summary>
+        /// <param name="statName"></param>
+        /// <param name="value"></param>
+        public void SetStat(string statName,float value)
+        {
+            _achievementProvider.SetStat(statName,value);
+        }
+
+        /// <summary>
+        /// Gets the value of the stat
+        /// </summary>
+        /// <param name="statName"></param>
+        /// <returns></returns>
+        public float GetStat(string statName)
+        {
+            return _achievementProvider.GetStat(statName);
+        }
+
+
+    }
+
+}
