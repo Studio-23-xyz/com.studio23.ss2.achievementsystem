cd842c1cdfc3f8d7b01ef0332f04bc88870cc777
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2023-12-15 15:00:09 +0600

    v1.1.8
    Added explicit isunlocked method

---
+++ Runtime/Core/AchievementProvider.cs
@@ -0,0 +1,18 @@
+using Studio23.SS2.AchievementSystem.Data;
+using System;
+using UnityEngine;
+
+namespace Studio23.SS2.AchievementSystem.Providers
+{
+	public  abstract class AchievementProvider : MonoBehaviour
+	{
+		[SerializeField] protected IDTableMapper _achievementMapper;
+        [SerializeField] protected IDTableMapper _statsMapper;
+        public AchievementData[] achievementDatas;
+        public abstract void Initialize();
+        public abstract AchievementData GetAchievement(string id);
+		public abstract void UpdateAchievementProgress(string id, float progression);
+ 
+
+    }
+}
