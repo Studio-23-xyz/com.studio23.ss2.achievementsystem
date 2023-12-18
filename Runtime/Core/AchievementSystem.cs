fa522596ae56c71bfef90b36983708da6f8b46a9
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2023-12-18 16:46:59 +0600

    v1.2.0

---
+++ Runtime/Core/AchievementSystem.cs
@@ -0,0 +1,78 @@
+using Studio23.SS2.AchievementSystem.Providers;
+using System;
+using UnityEngine;
+
+namespace Studio23.SS2.AchievementSystem.Core
+{
+    public delegate void InitializationEvent();
+    public class AchievementSystem : MonoBehaviour
+    {
+        public static AchievementSystem Instance;
+
+        public AchievementProvider _achievementProvider;
+
+
+        [SerializeField]private bool InitializeOnStart = true;
+
+        public InitializationEvent OnInitializeComplete;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            if (InitializeOnStart)
+            {
+                Initialize();
+            }
+        }
+
+
+        /// <summary>
+        /// Initialize Provider
+        /// </summary>
+        public void Initialize()
+        {
+            _achievementProvider = GetComponent<AchievementProvider>();
+            _achievementProvider.OnInitializationComplete += ()=> OnInitializeComplete?.Invoke();
+            _achievementProvider?.Initialize();
+        }
+
+    
+        public AchievementData GetAchievement(string id)
+        {
+            return _achievementProvider.GetAchievement(id);
+        }
+         
+
+        /// <summary>
+        /// Unlocks The achievement progression
+        /// </summary>
+        /// <param name="achievementName"></param>
+        /// <param name="progression"> progression is the unlock percentage of a achievement, the value will be withing 0 to 100</param>
+        
+        public  void UpdateAchievementProgress(string achievementName, float progression)
+        {
+           _achievementProvider.UpdateAchievementProgress(achievementName, progression);
+        }
+        
+        
+        /// <summary>
+        /// Unlocks The achievement
+        /// </summary>
+        /// <param name="achievementName"></param>
+
+        
+        public  void UnlockAchievement(string achievementName)
+        {
+            UpdateAchievementProgress(achievementName, 100);
+        }
+
+      
+
+
+    }
+
+}
